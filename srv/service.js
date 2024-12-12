51176467cb55582921142ceb9af66afd0c4e86a8
Milton Chandradas <milton.chandradas@sap.com>
2024-12-12 07:23:59 -0500

    initial commit...

--- srv/service.js
+++ srv/service.js
@@ -3,7 +3,7 @@ const { getCustomers } = require("./utils/northwindUtils");
 module.exports = async (srv) => {
   const { NorthwindCustomers } = srv.entities;
 
-  srv.on("READ", NorthwindCustomers, () => {
-    return getCustomers(srv);
+  srv.on("READ", NorthwindCustomers, async () => {
+    return await getCustomers(srv);
   });
 };
