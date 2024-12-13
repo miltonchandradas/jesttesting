65c3e0636409864ec10655869969429fd872a28b
Milton Chandradas <milton.chandradas@sap.com>
2024-12-13 08:29:26 -0500

    initial commit...

--- __test__/mockservice.test.js
+++ __test__/mockservice.test.js
@@ -1,14 +1,13 @@
 const cds = require("@sap/cds");
-const { getCustomers } = require("../srv/utils/northwindUtils");
+const northwindUtils = require("../srv/utils/northwindUtils");
 const customers = require("../northwindCustomers.json");
 
-jest.mock("../srv/utils/northwindUtils");
+jest.spyOn(northwindUtils, "getCustomers").mockImplementation(() => customers);
 
-describe("Jest Tests - Group 2", () => {
+describe("Jest Tests - MOCKING", () => {
   const { GET } = cds.test("serve", __dirname + "/../srv");
 
   it("Get Customers from Northwind OData Service - MOCKING", async () => {
-    getCustomers.mockImplementation(() => customers);
     const { status, data } = await GET(
       "/service/JestService/NorthwindCustomers"
     );
